666e8cf76d9512d318edb013cfbd0a223a7d5911
Azhar Mobeen <AzharMobeen@gmail.com>
2022-08-06 17:34:53 +0400

    Code Refactoring

--- src/main/java/ae/hcl/gateway/controller/CustomerController.java
+++ src/main/java/ae/hcl/gateway/controller/CustomerController.java
@@ -9,13 +9,11 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.security.SecurityRequirements;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.math.BigDecimal;
 import java.util.List;
 
--- src/main/java/ae/hcl/gateway/service/impl/CustomerServiceImpl.java
+++ src/main/java/ae/hcl/gateway/service/impl/CustomerServiceImpl.java
@@ -7,16 +7,12 @@ import ae.hcl.gateway.service.CustomerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 /**
